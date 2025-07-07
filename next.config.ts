bfbd16515f82f50a2ccb30b10b844e6201046b73
Christofer Henrique <christofer.almeida@r3suprimentos.com.br>
2025-07-07 16:17:11 -0300

    redirecionamento correto de URL

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
-  }
-  /* config options here */
+  },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "https://karcher-r3.com.br/",
+        permanent: true, // Redirecionamento 301 para SEO correto
+      },
+    ];
+  },
 };
 
 export default nextConfig;
